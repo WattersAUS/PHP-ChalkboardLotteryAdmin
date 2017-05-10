93723e6caf7116350272f813fd6dc01a2762a209
Graham Watson <grahamj.watson@gmail.com>
2017-05-10 18:07:17 +0100

    Update Lottery Class
    No returns JSON messages to PHP intermediate code (making Angular
    handling easier)

--- api/lottery.php
+++ api/lottery.php
@@ -7,6 +7,7 @@
 // Date       Version Note
 // ========== ======= ================================================
 // 2017-05-06 v0.01   First cut of code
+// 2017-05-10 v0.02   Use mySQLi not PDO, and return JSON result set
 //
 
 class Lottery {
@@ -28,132 +29,254 @@ class Lottery {
     public $isBonus;
     public $baseUrl;
     public $lastModified;
-    public $endDate
+    public $endDate;
+
+    public $numRows;
+    public $data;
 
     // constructor with $db as database connection
     public function __construct($db){
         $this->dbConnection = $db;
     }
 
-    function readAll() {
-        $query = "SELECT ident, description, draw, numbers, upper_number, numbers_tag, specials, upper_special, specials_tag, is_bonus, base_url, last_modified, end_date FROM ".$this->table_name;
-        $stmt  = $this->dbConnection->prepare($query);
-        $stmt->execute();
-        return $stmt;
-    }
-
-    function create() {
-        $query               = "INSERT INTO ".$this->table_name." SET description=:description, draw=:draw, numbers=:numbers, upper_number=:upper_number, numbers_tag=:numbers_tag, specials=:specials, upper_special=:upper_special, specials_tag=:specials_tag, is_bonus=:is_bonus, base_url=:base_url, last_modified=now(), end_date=NULL";
-        $stmt                = $this->dbConnection->prepare($query);
-        $this->description   = htmlspecialchars(strip_tags($this->description));
-        $this->base_url      = htmlspecialchars(strip_tags($this->base_url));
-        $stmt->bindParam(":description",   $this->description);
-        $stmt->bindParam(":draw",          $this->draw);
-        $stmt->bindParam(":numbers",       $this->numbers);
-        $stmt->bindParam(":upper_number",  $this->upper_number);
-        $stmt->bindParam(":numbers_tag",   $this->numbers_tag);
-        $stmt->bindParam(":specials",      $this->specials);
-        $stmt->bindParam(":upper_special", $this->upper_special);
-        $stmt->bindParam(":specials_tag",  $this->specials_tag);
-        $stmt->bindParam(":is_bonus",      $this->is_bonus);
-        $stmt->bindParam(":base_url",      $this->base_url);
-        if ($stmt->execute()) {
-            return true;
-        } else {
-            return false;
+    public function readAll() {
+        $data            = array();
+        $data["records"] = array();
+        $data["count"]   = 0;
+        $data["success"] = "Fail";
+        $query           = "SELECT ident, description, draw, numbers, upper_number, numbers_tag, specials, upper_special, specials_tag, is_bonus, base_url, last_modified, end_date FROM ".$this->table_name;
+        $result          = $this->dbConnection->query($query);
+        $this->numRows   = $result->num_rows;
+        if ($this->numRows > 0) {
+            while ($row = $result->fetch_assoc()){
+                $item=array(
+                    "ident"        => $row['ident'],
+                    "description"  => $row['description'],
+                    "draw"         => $row['draw'],
+                    "numbers"      => $row['numbers'],
+                    "upperNumber"  => $row['upper_number'],
+                    "numbersTag"   => $row['numbers_tag'],
+                    "specials"     => $row['specials'],
+                    "upperSpecial" => $row['upper_special'],
+                    "specialsTag"  => $row['specials_tag'],
+                    "isBonus"      => $row['is_bonus'],
+                    "baseUrl"      => $row['base_url'],
+                    "lastModified" => $row['last_modified'],
+                    "endDate"      => $row['end_date']
+                );
+                array_push($data["records"], $item);
+            }
+            $data["success"] = "Ok";
+            $data["count"]   = $this->numRows;
         }
+        return json_encode($data);
     }
 
-    function read() {
-        $query = "SELECT ident, description, draw, numbers, upper_number, numbers_tag, specials, upper_special, specials_tag, is_bonus, base_url, last_modified, end_date FROM ".$this->table_name." WHERE ident = ? LIMIT 0,1";
-        $stmt  = $this->dbConnection->prepare($query);
-        $stmt->bindParam(1, $this->ident);
-        $stmt->execute();
-        $row                 = $stmt->fetch(PDO::FETCH_ASSOC);
-        $this->description   = $row['description'];
-        $this->draw          = $row['draw'];
-        $this->numbers       = $row['numbers'];
-        $this->upper_number  = $row['upper_number'];
-        $this->numbers_tag   = $row['numbers_tag'];
-        $this->specials      = $row['specials'];
-        $this->upper_special = $row['upper_special'];
-        $this->specials_tag  = $row['specials_tag'];
-        $this->is_bonus      = $row['is_bonus'];
-        $this->base_url      = $row['base_url'];
-        $this->last_modified = $row['last_modified'];
-        $this->end_date      = $row['end_date'];
-    }
-
-    function update() {
-        $query               = "UPDATE ".$this->table_name." SET description=:description, draw=:draw, numbers=:numbers, upper_number=:upper_number, numbers_tag=:numbers_tag, specials=:specials, upper_special=:upper_special, specials_tag=:specials_tag, is_bonus=:is_bonus, base_url=:base_url, last_modified=now(), end_date=:end_date WHERE ident = :ident";
-        $stmt                = $this->dbConnection->prepare($query);
-        $this->description   = htmlspecialchars(strip_tags($this->description));
-        $this->base_url      = htmlspecialchars(strip_tags($this->base_url));
-        $stmt->bindParam(":description",   $this->description);
-        $stmt->bindParam(":draw",          $this->draw);
-        $stmt->bindParam(":numbers",       $this->numbers);
-        $stmt->bindParam(":upper_number",  $this->upper_number);
-        $stmt->bindParam(":numbers_tag",   $this->numbers_tag);
-        $stmt->bindParam(":specials",      $this->specials);
-        $stmt->bindParam(":upper_special", $this->upper_special);
-        $stmt->bindParam(":specials_tag",  $this->specials_tag);
-        $stmt->bindParam(":is_bonus",      $this->is_bonus);
-        $stmt->bindParam(":base_url",      $this->base_url);
-        $stmt->bindParam(":end_date",      $this->end_date);
-        $stmt->bindParam(":ident",         $this->ident);
+    public function read($id) {
+        $data            = array();
+        $data["records"] = array();
+        $data["count"]   = 0;
+        $data["success"] = "Fail";
+        $query           = "SELECT ident, description, draw, numbers, upper_number, numbers_tag, specials, upper_special, specials_tag, is_bonus, base_url, last_modified, end_date FROM ".$this->table_name." WHERE ident = ? LIMIT 0,1";
+        $stmt            = $this->dbConnection->prepare($query);
+        $stmt->bind_param('i', $id);
         if ($stmt->execute()) {
-            return true;
-        } else {
-            return false;
+            $result          = $stmt->get_result();
+            $this->numRows   = $result->num_rows;
+            if ($this->numRows > 0) {
+                while ($row = $result->fetch_assoc()){
+                    $item=array(
+                        "ident"        => $row['ident'],
+                        "description"  => $row['description'],
+                        "draw"         => $row['draw'],
+                        "numbers"      => $row['numbers'],
+                        "upperNumber"  => $row['upper_number'],
+                        "numbersTag"   => $row['numbers_tag'],
+                        "specials"     => $row['specials'],
+                        "upperSpecial" => $row['upper_special'],
+                        "specialsTag"  => $row['specials_tag'],
+                        "isBonus"      => $row['is_bonus'],
+                        "baseUrl"      => $row['base_url'],
+                        "lastModified" => $row['last_modified'],
+                        "endDate"      => $row['end_date']
+                    );
+                    array_push($data["records"], $item);
+                }
+            }
+            $data["success"] = "Ok";
+            $data["count"]   = $this->numRows;
         }
+        return json_encode($data);
     }
 
-    function disable() {
-        $query = "UPDATE ".$this->table_name." SET end_date = now() WHERE ident = :ident";
-        $stmt  = $this->dbConnection->prepare($query);
-        $stmt->bindParam(':ident', $this->ident);
+    public function search($keywords) {
+        $data            = array();
+        $data["records"] = array();
+        $data["count"]   = 0;
+        $data["success"] = "Fail";
+        $keywords        = htmlspecialchars(strip_tags($keywords));
+        $keywords        = "%{$keywords}%";
+        $query           = "SELECT ident, description, draw, numbers, upper_number, numbers_tag, specials, upper_special, specials_tag, is_bonus, base_url, last_modified, end_date FROM ".$this->table_name." WHERE description LIKE ? ORDER BY description DESC";
+        $stmt            = $this->dbConnection->prepare($query);
+        $stmt->bind_param('s', $keywords);
         if ($stmt->execute()) {
-            return true;
-        } else {
-            return false;
+            $result          = $stmt->get_result();
+            $this->numRows   = $result->num_rows;
+            if ($this->numRows > 0) {
+                while ($row = $result->fetch_assoc()){
+                    $item=array(
+                        "ident"        => $row['ident'],
+                        "description"  => $row['description'],
+                        "draw"         => $row['draw'],
+                        "numbers"      => $row['numbers'],
+                        "upperNumber"  => $row['upper_number'],
+                        "numbersTag"   => $row['numbers_tag'],
+                        "specials"     => $row['specials'],
+                        "upperSpecial" => $row['upper_special'],
+                        "specialsTag"  => $row['specials_tag'],
+                        "isBonus"      => $row['is_bonus'],
+                        "baseUrl"      => $row['base_url'],
+                        "lastModified" => $row['last_modified'],
+                        "endDate"      => $row['end_date']
+                    );
+                    array_push($data["records"], $item);
+                }
+            }
+            $data["success"] = "Ok";
+            $data["count"]   = $this->numRows;
         }
-    }
-
-    function enable() {
-        $query = "UPDATE ".$this->table_name." SET end_date = NULL WHERE ident = :ident";
-        $stmt  = $this->dbConnection->prepare($query);
-        $stmt->bindParam(':ident', $this->ident);
-        if ($stmt->execute()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    function search($keywords) {
-        $query = "SELECT ident, description, draw, numbers, upper_number, numbers_tag, specials, upper_special, specials_tag, is_bonus, base_url, last_modified, end_date FROM ".$this->table_name." WHERE description LIKE ? ORDER BY description DESC";
-        $stmt  = $this->dbConnection->prepare($query);
-        $keywords=htmlspecialchars(strip_tags($keywords));
-        $keywords = "%{$keywords}%";
-        $stmt->bindParam(1, $keywords);
-        $stmt->execute();
-        return $stmt;
-    }
-
-    public function pagingRead($from_record_num, $records_per_page) {
-        $query = "SELECT ident, description, draw, numbers, upper_number, numbers_tag, specials, upper_special, specials_tag, is_bonus, base_url, last_modified, end_date FROM ".$this->table_name." ORDER BY description DESC LIMIT ?, ?";
-        $stmt  = $this->dbConnection->prepare( $query );
-        $stmt->bindParam(1, $from_record_num, PDO::PARAM_INT);
-        $stmt->bindParam(2, $records_per_page, PDO::PARAM_INT);
-        $stmt->execute();
-        return $stmt;
+        return json_encode($data);
     }
 
     public function count() {
-        $query = "SELECT COUNT(*) as total_rows FROM " . $this->table_name . "";
-        $stmt  = $this->dbConnection->prepare( $query );
-        $stmt->execute();
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        return $row['total_rows'];
+        $data            = array();
+        $data["records"] = array();
+        $data["count"]   = 0;
+        $data["success"] = "Fail";
+        $query           = "SELECT COUNT(*) as total_rows FROM ".$this->table_name;
+        $result          = $this->dbConnection->query($query);
+        $this->numRows   = $result->num_rows;
+        if ($this->numRows > 0) {
+            if ($row = $result->fetch_assoc()) {
+                $data["success"] = "Ok";
+                $data["count"]   = $row['total_rows'];
+            }
+        }
+        return json_encode($data);
+    }
+
+    public function readPage($from, $count) {
+        $data            = array();
+        $data["records"] = array();
+        $data["count"]   = 0;
+        $data["success"] = "Fail";
+        $query           = "SELECT ident, description, draw, numbers, upper_number, numbers_tag, specials, upper_special, specials_tag, is_bonus, base_url, last_modified, end_date FROM ".$this->table_name." ORDER BY description DESC LIMIT ?, ?";
+        $stmt            = $this->dbConnection->prepare($query);
+        $stmt->bind_param('ii', $from, $count);
+        if ($stmt->execute()) {
+            $result          = $stmt->get_result();
+            $this->numRows   = $result->num_rows;
+            if ($this->numRows > 0) {
+                while ($row = $result->fetch_assoc()){
+                    $item=array(
+                        "ident"        => $row['ident'],
+                        "description"  => $row['description'],
+                        "draw"         => $row['draw'],
+                        "numbers"      => $row['numbers'],
+                        "upperNumber"  => $row['upper_number'],
+                        "numbersTag"   => $row['numbers_tag'],
+                        "specials"     => $row['specials'],
+                        "upperSpecial" => $row['upper_special'],
+                        "specialsTag"  => $row['specials_tag'],
+                        "isBonus"      => $row['is_bonus'],
+                        "baseUrl"      => $row['base_url'],
+                        "lastModified" => $row['last_modified'],
+                        "endDate"      => $row['end_date']
+                    );
+                    array_push($data["records"], $item);
+                }
+            }
+            $data["success"] = "Ok";
+            $data["count"]   = $this->numRows;
+        }
+        return json_encode($data);
+    }
+
+    public function insert() {
+        $query             = "INSERT INTO ".$this->table_name." SET description = ?, draw = ?, numbers = ?, upper_number = ?, numbers_tag = ?, specials = ?, upper_special = ?, specials_tag = ?, is_bonus = ?, base_url = ?, last_modified = now(), end_date = NULL";
+        $stmt              = $this->dbConnection->prepare($query);
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        $this->base_url    = htmlspecialchars(strip_tags($this->base_url));
+        $stmt->bind_param('siiisiisis', $this->description,$this->draw,$this->numbers,$this->upper_number,$this->numbers_tag,$this->specials,$this->upper_special,$this->specials_tag,$this->is_bonus,$this->base_url);
+        $data             = array();
+        $data["success"]  = "Fail";
+        $data["count"]    = 0;
+        $data["recordId"] = 0;
+        if ($stmt->execute()) {
+            $data["success"]  = "Ok";
+            $data["count"]    = $this->dbConnection->affected_rows;
+            $data["recordId"] = $this->dbConnection->insert_id;
+        }
+        return json_encode($data);
+    }
+
+    public function disable($id) {
+        $query = "UPDATE ".$this->table_name." SET end_date = now() WHERE ident = ?";
+        $stmt  = $this->dbConnection->prepare($query);
+        $stmt->bind_param('i', $id);
+        $data            = array();
+        $data["success"] = "Fail";
+        $data["count"]   = 0;
+        if ($stmt->execute()) {
+            $data["success"] = "Ok";
+            $data["count"]   = $this->dbConnection->affected_rows;
+        }
+        return json_encode($data);
+    }
+
+    public function enable($id) {
+        $query = "UPDATE ".$this->table_name." SET end_date = NULL WHERE ident = ?";
+        $stmt  = $this->dbConnection->prepare($query);
+        $stmt->bind_param('i', $id);
+        $data            = array();
+        $data["success"] = "Fail";
+        $data["count"]   = 0;
+        if ($stmt->execute()) {
+            $data["success"] = "Ok";
+            $data["count"]   = $this->dbConnection->affected_rows;
+        }
+        return json_encode($data);
+    }
+
+    public function delete($id) {
+        $query = "DELETE FROM ".$this->table_name." WHERE ident = ?";
+        $stmt  = $this->dbConnection->prepare($query);
+        $stmt->bind_param('i', $id);
+        $data            = array();
+        $data["success"] = "Fail";
+        $data["count"]   = 0;
+        if ($stmt->execute()) {
+            $data["success"] = "Ok";
+            $data["count"]   = $this->dbConnection->affected_rows;
+        }
+        return json_encode($data);
+    }
+
+    public function update($id) {
+        $query             = "UPDATE ".$this->table_name." SET description = ?, draw = ?, numbers = ?, upper_number = ?, numbers_tag = ?, specials = ?, upper_special = ?, specials_tag = ?, is_bonus = ?, base_url = ?, last_modified = now() WHERE ident = ?";
+        $stmt              = $this->dbConnection->prepare($query);
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        $this->base_url    = htmlspecialchars(strip_tags($this->base_url));
+        $stmt->bind_param('siiisiisisi', $this->description,$this->draw,$this->numbers,$this->upper_number,$this->numbers_tag,$this->specials,$this->upper_special,$this->specials_tag,$this->is_bonus,$this->base_url,$id);
+        $data            = array();
+        $data["success"] = "Fail";
+        $data["count"]   = 0;
+        if ($stmt->execute()) {
+            $data["success"] = "Ok";
+            $data["count"]   = $this->dbConnection->affected_rows;
+        }
+        return json_encode($data);
     }
 }
+
+?>
